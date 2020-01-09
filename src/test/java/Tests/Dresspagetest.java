f29728e429945b3e488710ace353cd3e482d4ecb
Kavya <kavyagopal1309@gmail.com>
2020-01-09 14:50:48 -0800

    kavya

--- src/test/java/Tests/Dresspagetest.java
+++ src/test/java/Tests/Dresspagetest.java
@@ -15,7 +15,7 @@ public class Dresspagetest {
 	Loginpageobject lg;
 	BasePage bp;
 
-	public Dresspagetest123() {
+	public Dresspagetest321() {
 		dp = new Dresspageobject();
 		bp = new BasePage();
 		lg = new Loginpageobject();
